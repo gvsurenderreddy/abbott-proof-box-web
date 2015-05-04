c17ec05356f7be8050bf18e93535e1165d9e7dea
Daniel Hall <daniel@danielhall.me>
2015-05-04 21:10:10 +1000

    Implement status

--- app/openvpn.lua
+++ app/openvpn.lua
@@ -1,10 +1,24 @@
 local openvpn = {}
 
 function openvpn.status(env, data)
+    local result = {}
+
+    local exit = os.execute('killall -0 openvpn')
+    if exit == 0 then
+        result.status = "running"
+    else
+        result.status = "stopped"
+    end
+
+    return result
+end
+
+function openvpn.config(env, data)
     return nil
 end
 
 register_path("^/v1/openvpn/?$", openvpn.status)
+register_path("^/v1/openvpn/config/?$", openvpn.config)
 
 return openvpn
 
